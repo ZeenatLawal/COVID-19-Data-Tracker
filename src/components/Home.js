c472e681c10675243443519c703a79c690762dea
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-13 09:28:27 +0100

    Display all countries on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
+/* eslint-disable dot-notation */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Navbar from './Navbar';
+import CountryCard from './Card';
+import TotalCases from './TotalCases';
 
-const Home = () => (
-  <div>
-    <Navbar title="All cases" left="2021" />
-    <h1>Home</h1>
-  </div>
-);
+const Home = () => {
+  const countries = useSelector((state) => state.countriesReducer);
+
+  return (
+    <div>
+      <Navbar title="All Africa cases" left="2021" />
+      <div>
+        <div>
+          <TotalCases />
+        </div>
+        {countries.map((country) => (
+          <CountryCard key={country['All'].country} country={country} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
