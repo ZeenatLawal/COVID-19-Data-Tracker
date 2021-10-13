ee912615437bdb49d092252929d2eae49a0c34fe
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-13 11:41:05 +0100

    Make the whole card component clickable on homepage

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,19 +2,22 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import { FiArrowRightCircle } from 'react-icons/fi';
 
 const CountryCard = ({ country }) => {
   const page = `/details/${country['All'].country}`;
 
   return (
     <Card key={country['All'].country} className="card">
-      <Card.Title>{country['All'].country}</Card.Title>
-      <Card.Body>
-        <Card.Text>
-          {country['All'].confirmed}
-        </Card.Text>
-      </Card.Body>
-      <Card.Link href={page}>Details</Card.Link>
+      <Card.Link href={page}>
+        <FiArrowRightCircle />
+        <Card.Title>{country['All'].country}</Card.Title>
+        <Card.Body>
+          <Card.Text>
+            {country['All'].confirmed}
+          </Card.Text>
+        </Card.Body>
+      </Card.Link>
     </Card>
   );
 };
