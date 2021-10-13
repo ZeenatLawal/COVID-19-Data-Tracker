767b70769c9062f54078fedc01f188ecd0b735db
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-13 11:42:04 +0100

    Calculate and display total confirmed cases on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,24 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Navbar from './Navbar';
 import CountryCard from './Card';
-import TotalCases from './TotalCases';
+import TotalCard from './TotalCard';
 
 const Home = () => {
   const countries = useSelector((state) => state.countriesReducer);
+
+  let total = 0;
+  if (countries) {
+    countries.forEach((element) => {
+      total += element['All'].confirmed;
+    });
+  }
 
   return (
     <div>
       <Navbar title="All Africa cases" left="2021" />
       <div>
         <div>
-          <TotalCases />
+          <TotalCard name="Africa Total confirmed:" total={total} />
         </div>
         {countries.map((country) => (
           <CountryCard key={country['All'].country} country={country} />
