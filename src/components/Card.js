d2de6d230225d542c2ebab7c8c7e0cde844e17db
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-13 22:19:56 +0100

    Add styling to Homepage

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,20 +3,26 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { FiArrowRightCircle } from 'react-icons/fi';
+import covid from '../assets/covid.svg';
 
 const CountryCard = ({ country }) => {
   const page = `/details/${country['All'].country}`;
 
   return (
-    <Card key={country['All'].country} className="card w-50 border-0">
+    <Card key={country['All'].country} className="card border-0 p-2">
       <Card.Link href={page}>
-        <FiArrowRightCircle />
-        <Card.Title>{country['All'].country}</Card.Title>
-        <Card.Body>
-          <Card.Text>
-            {country['All'].confirmed}
-          </Card.Text>
-        </Card.Body>
+        <div className="d-flex justify-content-between ps-3">
+          <img src={covid} alt="covid virus" className="map w-50 h-50" />
+          <FiArrowRightCircle />
+        </div>
+        <div className="text-end">
+          <Card.Title>{country['All'].country}</Card.Title>
+          <Card.Body>
+            <Card.Text>
+              {country['All'].confirmed}
+            </Card.Text>
+          </Card.Body>
+        </div>
       </Card.Link>
     </Card>
   );
