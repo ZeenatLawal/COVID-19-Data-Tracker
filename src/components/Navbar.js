5a46661efb7e2432189cbbd1f6842c563d85b968
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-13 16:41:43 +0100

    Add PropTypes for Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { MdMic, MdSettings } from 'react-icons/md';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = (props) => {
-  if (props.title === 'Country cases') {
+  const { title, left } = props;
+  if (title === 'Country cases') {
     return (
       <div className="d-flex justify-content-between align-items-center navbar">
-        <NavLink to="/" exact>{props.left}</NavLink>
-        <p>{props.title}</p>
+        <NavLink to="/" exact>{left}</NavLink>
+        <p>{title}</p>
         <div className="fs-4">
           <MdMic className="me-3" />
           <MdSettings />
@@ -19,14 +19,22 @@ const Navbar = (props) => {
   }
   return (
     <div className="d-flex justify-content-between align-items-center navbar">
-      <p>{props.left}</p>
-      <p>{props.title}</p>
+      <p>{left}</p>
+      <p>{title}</p>
       <div className="fs-4">
         <MdMic className="me-3" />
         <MdSettings />
       </div>
     </div>
   );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  left: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+  ]).isRequired,
 };
 
 export default Navbar;
