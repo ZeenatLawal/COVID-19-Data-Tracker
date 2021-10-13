6cbe7413f6183688c8f77d24ba0870238e437c02
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-13 09:28:48 +0100

    Modify Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,32 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import PropTypes from 'prop-types';
 import { MdMic, MdSettings } from 'react-icons/md';
+import { NavLink } from 'react-router-dom';
 
-const Navbar = (props) => (
-  <div className="d-flex justify-content-between align-items-center navbar">
-    <p>{props.left}</p>
-    <p>{props.title}</p>
-    <div className="fs-4">
-      <MdMic className="me-3" />
-      <MdSettings />
+const Navbar = (props) => {
+  if (props.title === 'Country cases') {
+    return (
+      <div className="d-flex justify-content-between align-items-center navbar">
+        <NavLink to="/" exact>{props.left}</NavLink>
+        <p>{props.title}</p>
+        <div className="fs-4">
+          <MdMic className="me-3" />
+          <MdSettings />
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="d-flex justify-content-between align-items-center navbar">
+      <p>{props.left}</p>
+      <p>{props.title}</p>
+      <div className="fs-4">
+        <MdMic className="me-3" />
+        <MdSettings />
+      </div>
     </div>
-  </div>
-);
-
-Navbar.propTypes = {
-  title: PropTypes.string.isRequired,
-  left: PropTypes.string.isRequired,
+  );
 };
 
 export default Navbar;
