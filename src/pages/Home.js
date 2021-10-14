77e9f354e1ed81c46a1fc1300d6f8b82bc4a9984
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-14 21:37:45 +0100

    Use dot notation in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,3 @@
-/* eslint-disable dot-notation */
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Navbar from '../components/Navbar';
@@ -15,7 +14,7 @@ const Home = () => {
   let total = 0;
   if (countries) {
     countries.forEach((element) => {
-      total += element['All'].confirmed;
+      total += element.All.confirmed;
     });
   }
 
@@ -37,7 +36,7 @@ const Home = () => {
         </div>
         <div className="d-flex flex-wrap">
           {countries && countries.map((country) => (
-            <CountryCard key={country['All'].country} country={country} />
+            <CountryCard key={country.All.country} country={country} />
           ))}
         </div>
       </div>
