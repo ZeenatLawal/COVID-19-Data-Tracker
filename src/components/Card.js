28357008c6e26edb1b030b32b2b75a162e03a972
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-14 21:32:17 +0100

    Add propTypes validation to CountryCard component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,39 @@
-/* eslint-disable dot-notation */
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import { FiArrowRightCircle } from 'react-icons/fi';
 import covid from '../assets/covid.svg';
 
 const CountryCard = ({ country }) => {
-  const page = `/details/${country['All'].country}`;
+  const page = `/details/${country.All.country}`;
 
   return (
-    <Card key={country['All'].country} className="card border-0 p-2 country-card">
+    <Card key={country.All.country} className="card border-0 p-2 country-card">
       <Card.Link href={page}>
         <div className="d-flex justify-content-between ps-3">
           <img src={covid} alt="covid virus" className="map w-50 h-50" />
           <FiArrowRightCircle />
         </div>
         <div className="text-end">
-          <Card.Title>{country['All'].country}</Card.Title>
+          <Card.Title>{country.All.country}</Card.Title>
           <Card.Body>
             <Card.Text>
-              {country['All'].confirmed.toLocaleString()}
+              {country.All.confirmed.toLocaleString()}
             </Card.Text>
           </Card.Body>
         </div>
       </Card.Link>
     </Card>
   );
+};
+
+CountryCard.propTypes = {
+  country: PropTypes.shape({
+    All: PropTypes.shape({
+      country: PropTypes.string,
+      confirmed: PropTypes.number,
+    }),
+  }).isRequired,
 };
 
 export default CountryCard;
